10357dc14efc18173a3a2111607750bd47e52230
riahikais <riahikais@yahoo.com>
2021-01-15 13:48:37 +0100

    Guest order function

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from .utils import cookieCart , cartData
+from .utils import cookieCart, cartData, guestOrder
 from django.http import JsonResponse
 # Create your views here.
 # import the logging library
@@ -74,30 +74,7 @@ def processOrder(request):
         order, created = Order.objects.get_or_create(customer=customer, complete= False)
 
     else:
-        print('User is not logged in..')
-        print ('COOKIES : ', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(
-            email = email
-        )
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(
-            customer=customer,
-            complete=False
-        )
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity']
-            )
+        customer, order = guestOrder(request, data)
 
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
