cf67a4d486ab9d2cf92765443160a2d9b39d4322
riahikais <riahikais@yahoo.com>
2021-01-15 12:08:44 +0100

    Render cart total

--- store/views.py
+++ store/views.py
@@ -34,9 +34,16 @@ def cart(request):
         items =order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
+        for i in cart:
+            cartItems += cart[i]['quantity']
+
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
